3c19d1d216bb1c09b506a42ae3b5b0ca18e270db
unknown <fowzan.alpha@gmail.com>
2021-06-13 01:06:33 +0300

    Optimized Performance using Lazy Loading

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,33 +1,45 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "../../styles/Main/Main.css";
-
-import Home from "./HomePage/Home";
-import Genre from "./GenrePage/Genre";
-import Popular from "./PopularPage/Popular";
-import TopRated from "./TopRatedPage/TopRated";
-import Upcoming from "./UpcomingPage/Upcoming";
-import Movie from "./MoviePage/Movie";
-import Celebrity from "./CelebrityPage/Celebrity";
-import Favourite from "./FavouritePage/Favourite";
-import NotFoundPage from "./NotFoundPage";
 import { Container } from "react-bootstrap";
 
+const Home = React.lazy(() => import("./HomePage/Home"));
+const Genre = React.lazy(() => import("./GenrePage/Genre"));
+const Popular = React.lazy(() => import("./PopularPage/Popular"));
+const TopRated = React.lazy(() => import("./TopRatedPage/TopRated"));
+const Upcoming = React.lazy(() => import("./UpcomingPage/Upcoming"));
+const Movie = React.lazy(() => import("./MoviePage/Movie"));
+const Celebrity = React.lazy(() => import("./CelebrityPage/Celebrity"));
+const Favourite = React.lazy(() => import("./FavouritePage/Favourite"));
+const NotFoundPage = React.lazy(() => import("./NotFoundPage"));
+
 function Main() {
+
+const Loading=()=>{
+  return(
+    <>
+    <p>Loading</p>
+    </>
+  )
+}
+
+
   return (
     <div className="main">
       <Container>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/genre" component={Genre} />
-          <Route path="/popular" component={Popular} />
-          <Route path="/toprated" component={TopRated} />
-          <Route path="/upcoming" component={Upcoming} />
-          <Route path="/movie/:id" component={Movie} />
-          <Route path="/celebrity/:id" component={Celebrity} />
-          <Route path="/favourite" component={Favourite} />
-          <Route component={NotFoundPage} />
-        </Switch>
+        <React.Suspense fallback={Loading}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/genre" component={Genre} />
+            <Route path="/popular" component={Popular} />
+            <Route path="/toprated" component={TopRated} />
+            <Route path="/upcoming" component={Upcoming} />
+            <Route path="/movie/:id" component={Movie} />
+            <Route path="/celebrity/:id" component={Celebrity} />
+            <Route path="/favourite" component={Favourite} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </React.Suspense>
       </Container>
     </div>
   );
